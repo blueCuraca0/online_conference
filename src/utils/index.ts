61bd236c212f0c54bc5b33e385ff518eda907607
Veronika <vze@empat.tech>
2026-05-10 19:09:44 +0200

    fix: conference redirect link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,5 @@ export const appEnvironment = process.env.REACT_APP_ENVIRONMENT;
 
 export const passwordValidationRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-export const CONFERENCE_LINK_BASE = "localhost:3000/conference/";
+export const CONFERENCE_LINK_BASE = `${apiBaseUrl}/conference/`;
 // export const STUB_LINK = "https://confly.app/r/quiet-river-204";
